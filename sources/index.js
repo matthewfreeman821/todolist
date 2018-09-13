7dc2e0c94c1f558ae61f8b2d84d74154aa5768e6
Matthew Freeman <matthew.freeman821@gmail.com>
2018-09-13 15:50:10 -0400

    Updated js file to include a window timeout in order for html page to load prior to JavaScript loading

--- sources/index.js
+++ sources/index.js
@@ -1,35 +1,37 @@
-var todos = [];//Initializes empty array to add todos
+window.setTimeout(function() {
+    var todos = [];//Initializes empty array to add todos
 
-var input = prompt('What would you like to do? ');//Prompts user to input what they would like to do from the list generated in the html file
+    var input = prompt('What would you like to do? ');//Prompts user to input what they would like to do from the list generated in the html file
 
-while (input !== 'quit') {//Allows the app to continue to prompt user until quit is entered
-    if (input === 'list') {//Allows user to access the list of todos
-        listTasks();
-    } else if (input === 'new') {
-        addTasks();
-    } else if (input === 'delete') {
-        removeTasks();
+    while (input !== 'quit') {//Allows the app to continue to prompt user until quit is entered
+        if (input === 'list') {//Allows user to access the list of todos
+            listTasks();
+        } else if (input === 'new') {
+            addTasks();
+        } else if (input === 'delete') {
+            removeTasks();
+        }
+        input = prompt('What would you like to do? ');
     }
-    input = prompt('What would you like to do? ');
-}
-console.log('OK, you quit the app');
+    console.log('OK, you quit the app');
 
-function listTasks () {
-    console.log('***************')
-    todos.forEach(function(todo, i) {
-        console.log(i + ': ' + todo);
-    });
-    console.log('***************');
-}
+    function listTasks () {
+        console.log('***************')
+        todos.forEach(function(todo, i) {
+            console.log(i + ': ' + todo);
+        });
+        console.log('***************');
+    }
 
-function addTasks() {
-    var newTodo = prompt('Enter new task');
-        todos.push(newTodo);
-        console.log('Added Task');
-}
+    function addTasks() {
+        var newTodo = prompt('Enter new task');
+            todos.push(newTodo);
+            console.log('Added Task');
+    }
 
-function removeTasks () {
-    var index = promot('Enter index of task to delete');
-    todos.splice(index, 1);
-    console.log('Deleted Task');
-}
+    function removeTasks () {
+        var index = promot('Enter index of task to delete');
+        todos.splice(index, 1);
+        console.log('Deleted Task');
+    }
+}, 500);
